317decdb3f2147b227b159ea7a837fac6175e8ef
huangchen <huangchen_123@qq.com>
2023-06-02 14:01:59 +0800

    新增setnoblocking函数和addfd函数

--- include/utils.h
+++ include/utils.h
@@ -4,7 +4,16 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <sys/epoll.h>
 
+// 错误处理函数
 void error_handle(char msg[]);
+
+// 将文件描述符设置成非阻塞的,返回原先的文件描述符选项
+int setnoblocking(int fd);
+
+// 将文件描述符fd注册到epollfd内核事件表中，监听事件为EPOLLIN，参数enable_et指定是否启用边缘触发
+void addfd(int epollfd, int fd, bool enable_et);
 
 #endif
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,7 +1,28 @@
 #include "utils.h"
 
+// 错误处理函数
 void error_handle(char msg[])
 {
     printf("%s\n", msg);
     exit(-1);
+}
+
+// 将文件描述符设置成非阻塞的,返回原先的文件描述符选项
+int setnoblocking(int fd)
+{
+    int old_opt = fcntl(fd, F_GETFL);
+    fcntl(fd, F_SETFL, old_opt | O_NONBLOCK);
+    return old_opt;
+}
+
+// 将文件描述符fd注册到epollfd内核事件表中，监听事件为EPOLLIN，参数enable_et指定是否启用边缘触发
+void addfd(int epollfd, int fd, bool enable_et)
+{
+    epoll_event event;
+    event.data.fd = fd;
+    event.events = EPOLLIN;
+    if (enable_et)
+        event.events |= EPOLLET;
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+    setnoblocking(fd);
 }
