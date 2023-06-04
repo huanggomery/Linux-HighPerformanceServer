59674da65db57d397f181313775ff3fb51cb0439
huangchen <huangchen_123@qq.com>
2023-06-04 14:56:37 +0800

    新增客户端连接函数connect_with_timeout

--- include/utils.h
+++ include/utils.h
@@ -2,10 +2,6 @@
 #ifndef _UTILS_H
 #define _UTILS_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <sys/epoll.h>
 
 // 错误处理函数
 void error_handle(char msg[]);
@@ -15,5 +11,8 @@ int setnoblocking(int fd);
 
 // 将文件描述符fd注册到epollfd内核事件表中，监听事件为EPOLLIN，参数enable_et指定是否启用边缘触发
 void addfd(int epollfd, int fd, bool enable_et);
+
+// 创建客户端socket并连接服务器，服务器地址默认为127.0.0.1:9190，超时时间默认5s，成功则返回sockfd，否则返回-1
+int connect_with_timeout(const char *ip = nullptr, int port = 0, int time = 5);
 
 #endif
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,4 +1,12 @@
 #include "utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/epoll.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <errno.h>
 
 // 错误处理函数
 void error_handle(char msg[])
@@ -25,4 +33,46 @@ void addfd(int epollfd, int fd, bool enable_et)
         event.events |= EPOLLET;
     epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
     setnoblocking(fd);
+}
+
+// 创建客户端socket并连接服务器，服务器地址默认为127.0.0.1:9190，超时时间默认5s，成功则返回sockfd，否则返回-1
+int connect_with_timeout(const char *ip, int port, int time)
+{
+    if (port == 0)
+        port = 9190;
+    struct sockaddr_in address;
+    memset(&address, 0, sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr(ip ? ip : "127.0.0.1");
+    address.sin_port = htons(port);
+
+    int sockfd = socket(PF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1)
+    {
+        printf("socket error\n");
+        return -1;
+    }
+
+    struct timeval timeout;
+    timeout.tv_sec = time;
+    timeout.tv_usec = 0;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_SNDTIMEO, &timeout, sizeof(timeout)) == -1)
+    {
+        printf("setsockopt error");
+        return -1;
+    }
+
+    int ret = connect(sockfd, (sockaddr *)&address, sizeof(address));
+    if (ret == -1)
+    {
+        // 连接超时
+        if (errno == EINPROGRESS)
+        {
+            printf("connecting timeout\n");
+            return -1;
+        }
+        printf("error occur when connecting to server\n");
+        return -1;
+    }
+    return sockfd;
 }
